b4050ec4efdfa0879fdfad0b39617520ee18cd6a
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-03-24 18:11:47 +0530

    added giveaway time

--- app.js
+++ app.js
@@ -26,3 +26,21 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline h4');
 
+const futureDate = new Date();
+
+// console.log(futureDate);
+
+let weekday = futureDate.getDay();
+weekday = weekdays[weekday];
+
+const date = futureDate.getDate();
+let month = futureDate.getMonth();
+month = months[month];
+const year = futureDate.getFullYear();
+
+const hrs = futureDate.getHours();
+const mins = futureDate.getMinutes();
+const time = `${hrs}:${mins}`;
+console.log(time);
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${time}am`;
